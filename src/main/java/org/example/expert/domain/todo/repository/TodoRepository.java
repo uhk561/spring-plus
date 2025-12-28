3bd73123f81a41cc485ecfb56c6e6bfae617b4fa
sungwan <uhk561@naver.com>
2025-12-29 00:45:31 +0900

    Revert "Lv.1-3 검색조건 구현 완료"
    This reverts commit 7894aa33a0b56ed1135c7c69e29c6c47dd99a60c.

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -19,71 +18,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
-
-    // 날씨만
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.weather = :weather
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByWeather(@Param("weather") String weather, Pageable pageable);
-
-    // 시작일 종료일
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.modifiedAt BETWEEN :start AND :end
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByModifiedAtBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, Pageable pageable);
-
-    // 시작일
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.modifiedAt >= :start
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByModifiedAtAfter(@Param("start") LocalDateTime start, Pageable pageable);
-
-    // 종료일
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.modifiedAt <= :end
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByModifiedAtBefore(@Param("end") LocalDateTime end, Pageable pageable);
-
-    // 날씨 + 시작일 종료일
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.weather = :weather
-                AND t.modifiedAt BETWEEN :start AND :end
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByWeatherAndModifiedAtBetween(@Param("weather") String weather, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end, Pageable pageable);
-
-
-    // 날씨 + 시작일
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.weather = :weather
-                AND t.modifiedAt >= :start
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByWeatherAndModifiedAtAfter(@Param("weather") String weather, @Param("start") LocalDateTime start, Pageable pageable);
-
-    // 날씨 + 종료일
-    @Query("""
-                SELECT t
-                FROM Todo t
-                WHERE t.weather = :weather
-                AND t.modifiedAt <= :end
-                ORDER BY t.modifiedAt DESC
-            """)
-    Page<Todo> findByWeatherAndModifiedAtBefore(@Param("weather") String weather, @Param("end") LocalDateTime end, Pageable pageable);
 }
